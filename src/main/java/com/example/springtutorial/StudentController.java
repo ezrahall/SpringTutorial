3845e9d35ca5749a7de529e9b9278f6d96ee47eb
Ezra Hall <ezramackhall@gmail.com>
2021-12-08 15:08:48 -0600

    Simple Get API to learn Spring Boot

--- src/main/java/com/example/springtutorial/StudentController.java
+++ src/main/java/com/example/springtutorial/StudentController.java
@@ -1,13 +1,43 @@
 package com.example.springtutorial;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StudentController {
 
+    //Return Object
     @GetMapping("/student")
     public Student getStudent(){
         return new Student("Ezra", "Hall");
+    }
+
+    //List
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Ezra", "Hall"));
+        students.add(new Student("Student", "Two"));
+        students.add(new Student("Student", "Three"));
+        students.add(new Student("Student", "Four"));
+        return students;
+    }
+
+    //Path Parameters
+    @GetMapping("student/{firstName}/{lastName}")
+    public Student studentPathVariable(@PathVariable("firstName") String firstName,
+                                       @PathVariable("lastName") String lastName){
+        return new Student(firstName, lastName);
+    }
+
+    @GetMapping("/studentQuery")
+    public Student studentQueryParam(@RequestParam("firstName") String firstName,
+                                     @RequestParam("lastName") String lastName){
+        return new Student(firstName, lastName);
     }
 }
